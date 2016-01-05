11ce7f0ae86f49724d5a6dd372e30d42ed5c0a75
Oriol Collell <oriol.collell.bcn@gmail.com>
2016-01-05 18:18:50 +0100

    allow completion for aliases

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,6 +4,4 @@ export CLICOLOR=true
 fpath=($DOTFILES/functions $fpath)
 autoload -U $DOTFILES/functions/*(:t)
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+setopt no_complete_aliases
